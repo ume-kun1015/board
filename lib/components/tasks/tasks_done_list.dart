732ed4e4a1144e5f1b1ea76c9333a931b090839b
romukey <baymax.blackandwhite@gmail.com>
2021-11-27 22:00:15 +0900

    feat: prepare tasks provider and list for each task status

---
+++ lib/components/tasks/tasks_done_list.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import 'package:board/components/tasks/task_card.dart';
+import 'package:board/state/tasks_state.dart';
+
+class TasksDoneList extends ConsumerWidget {
+  const TasksDoneList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final tasks = ref.watch(tasksProvider);
+
+    return ListView.builder(
+      itemCount: tasks.length,
+      padding: const EdgeInsets.only(bottom: 60.0),
+      itemBuilder: (BuildContext context, int index) {
+        return TaskCard(
+          id: tasks[index].id,
+          title: tasks[index].title,
+          description: tasks[index].description,
+          dueDateTime: tasks[index].dueDateTime,
+          status: tasks[index].status,
+        );
+      },
+    );
+  }
+}

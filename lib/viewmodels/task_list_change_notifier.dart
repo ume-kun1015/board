5ff479281cfe58da243a903b4cf34692d02fc23b
romukey <baymax.blackandwhite@gmail.com>
2021-11-28 23:36:09 +0900

    feat: rename to task_list_change_notifier and return tasks list

---
+++ lib/viewmodels/task_list_change_notifier.dart
@@ -0,0 +1,16 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import 'package:board/repositories/task_repository.dart';
+import 'package:board/models/task_model.dart';
+
+class TaskListChangeNotifier extends StateNotifier<List<TaskModel>> {
+  TaskListChangeNotifier({required this.repository}) : super([]);
+
+  final TaskRepository repository;
+
+  Future<void> findByStatus(String status) async {
+    return repository.findByStatus(status).then((value) {
+      state = value;
+    }).catchError((dynamic error) {});
+  }
+}

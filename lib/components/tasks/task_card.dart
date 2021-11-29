1acf299139069cba7a33d7a41f46951c7ce0186c
romukey <baymax.blackandwhite@gmail.com>
2021-11-29 19:05:28 +0900

    fix: fix padding

--- lib/components/tasks/task_card.dart
+++ lib/components/tasks/task_card.dart
@@ -58,7 +58,7 @@ class TaskCard extends ConsumerWidget {
                   ),
                 ),
                 Container(
-                  padding: const EdgeInsets.all(6.0),
+                  padding: const EdgeInsets.all(8.0),
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(10),
                     color: statusColor(),
